3d4431b307a2f7e7fbf17c1bcdc600380637228c
bibidibovidiboo <66943451+bibidibovidiboo@users.noreply.github.com>
2020-10-12 18:12:19 +0900

    OnLineStudy

--- OnLineStudy16_MVC1/src/com/sist/model/InSertModel.java
+++ OnLineStudy16_MVC1/src/com/sist/model/InSertModel.java
@@ -1,13 +1,13 @@
 package com.sist.model;
 
 import javax.servlet.http.HttpServletRequest;
-
-public class InSertModel implements Model {
+// 글쓰기 요청시 처리 
+public class InsertModel implements Model{
 
 	@Override
 	public String handlerRequest(HttpServletRequest request) {
 		// TODO Auto-generated method stub
-		return null;
+		return "board/insert.jsp";// 화면만 이동 
 	}
 
 }
